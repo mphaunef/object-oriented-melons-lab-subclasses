f3074b3a7adce4d919a6b4653d1da41731638bc9
Megan <meganphaunef14@yahoo.com>
2022-04-08 15:09:19 -0400

    Updated methods to include surge pricing and rush hour prices

--- melons.py
+++ melons.py
@@ -1,4 +1,6 @@
 """Classes for melon orders."""
+from random import choice
+import datetime
 
 class AbstractMelonOrder:
     """Super class of Melon orders"""
@@ -13,10 +15,31 @@ class AbstractMelonOrder:
         # self.country_code = country_code 
         # self.shipped = False
 
+    # @staticmethod
+    def get_base_price(self):
+        """Calculate splurge base pricing"""
+
+        base_price = choice(range(5,10)) 
+        # time = datetime.time()
+        # now = datetime.datetime.now()
+
+        # # Is it rush hour?
+        # if now.hour >= 8 and now.hour <= 11 and now.weekday() < 5:
+        #     base_price += 4
+
+    
+        order_time = datetime.datetime.now()
+        day_of_week = datetime.datetime.weekday(order_time)
+
+        if (day_of_week < 5) and (order_time.hour >= 8 and order_time.hour <= 11): 
+                base_price += 4
+
+        return base_price 
+
     def get_total(self):
         """Calculate price, including tax."""
 
-        base_price = 5
+        base_price = self.get_base_price()
         # total = (1 + self.tax) * self.qty * base_price
         
         if self.species == 'Christmas melon':
@@ -49,6 +72,21 @@ class DomesticMelonOrder(AbstractMelonOrder):
     #     super().__init__(species, qty, "domestic", 0.08)
 
 
+class GovernmentMelonOrder(AbstractMelonOrder): 
+    """Ubermelon has struck a deal with the US Government."""
+
+    # tax = 0.00 
+    passed_inspection = False 
+    
+    def __init__(self, species, qty):
+        """Initialize melon order attributes."""
+        # self.passed_inspection = False
+        super().__init__(species, qty, order_type='Government', tax=0.00)
+ 
+    def mark_inspection(self, passed):
+        # if passed == bool('True'): 
+            self.passed_inspection = passed
+
 
 
 class InternationalMelonOrder(AbstractMelonOrder):
@@ -67,3 +105,5 @@ class InternationalMelonOrder(AbstractMelonOrder):
         """Return the country code."""
 
         return self.country_code
+
+
